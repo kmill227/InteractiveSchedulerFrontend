874df1410a94107d5931fa0b0a24d2fe94974a30
kyleondecker@gmail.com <68393824+kyleondecker@users.noreply.github.com>
2023-03-14 11:04:26 -0400

    Added prject name to top bar

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import './App.css';
+import './navbar.css';
 import { Box, List, Divider, ListItem, ListItemIcon, ListItemText, Hidden, Drawer, Typography, AppBar, Toolbar, IconButton } from '@material-ui/core';
 import PersonIcon from '@material-ui/icons/Person';
 import HomeIcon from '@material-ui/icons/Home';
@@ -12,38 +13,40 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { ListItemButton } from '@mui/material';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CssBaseline from '@mui/material/CssBaseline';
+import { Calendar } from 'react-big-calendar';
+import MyCalendar from './CalendarApp';
 
                
-                const drawerWidth = 240;
-                
-                export default function PermanentDrawerLeft() { 
-                  return (
-                    <Box sx={{ display: 'flex' }}>
-                      <CssBaseline />
-                      <AppBar
-                        position="fixed"
-                        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-                      >
-                        <Toolbar>
-                          <Typography variant="h6" noWrap component="div">
-                            FlashFriends
-                          </Typography>
-                        </Toolbar>
-                      </AppBar>
-                      <Drawer
-                        sx={{
-                          width: drawerWidth,
-                          flexShrink: 0,
-                          '& .MuiDrawer-paper': {
-                            width: drawerWidth,
-                            boxSizing: 'border-box',
-                          },
-                        }}
-                        variant="permanent"
-                        anchor="left"
-                      >
-                        <Toolbar />
-                        <Divider />
+const drawerWidth = 120;
+
+export default function PermanentDrawerLeft() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Flash Friends
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
                         <List>
                     <ListItem>
                         <ListItemIcon>
@@ -99,5 +102,115 @@ import CssBaseline from '@mui/material/CssBaseline';
                         <Toolbar />
                       </Box>
                     </Box>
+                    
                   );
                 }
+*/
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import PersonIcon from '@material-ui/icons/Person';
+import HomeIcon from '@material-ui/icons/Home';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import EmailIcon from '@mui/icons-material/Email';
+import Help from '@material-ui/icons/Help';
+
+const drawerWidth = 240;
+
+export default function PermanentDrawerLeft() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Flash Friends
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+        <List>
+        <ListItem>
+                        <ListItemIcon>
+                            <HomeIcon />
+                        </ListItemIcon>
+                        <ListItemButton component="a" href="/">
+                        <ListItemText primary="Home" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <CalendarMonthIcon />
+                        </ListItemIcon>
+                        <ListItemButton component="a" href="./CalendarApp">
+                        <ListItemText primary="Calendar" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <EmailIcon />
+                        </ListItemIcon>
+                        <ListItemButton component="a" href="./Messages">
+                        <ListItemText primary="Messages" />
+                        </ListItemButton>
+                    </ListItem>
+        </List>
+        <Divider />
+        <List>
+        <ListItem>
+                        <ListItemIcon>
+                            <HomeIcon />
+                        </ListItemIcon>
+                        <ListItemButton component="a" href="/Login">
+                        <ListItemText primary="Login" />
+                        </ListItemButton>
+                    </ListItem>
+                    <Divider />
+                    <ListItem>
+                        <ListItemIcon>
+                            <CalendarMonthIcon />
+                        </ListItemIcon>
+                        <ListItemButton component="a" href="./Support">
+                        <ListItemText primary="Support" />
+                        </ListItemButton>
+                    </ListItem>
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+      >
+        <Toolbar />
+        
+      </Box>
+    </Box>
+  );
+}
